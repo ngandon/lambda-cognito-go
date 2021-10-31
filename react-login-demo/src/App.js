fa78edfb30e93a3af616b8f96d138b9ce2ad5514
ngandon <nicolas.gandon78@gmail.com>
2021-10-31 20:10:40 +0100

    change class components to hooks

--- react-login-demo/src/App.js
+++ react-login-demo/src/App.js
@@ -1,29 +1,57 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import SignInForm from "./pages/SignInForm";
 import { AppContext } from "./lib/contextLib";
-
 import "./App.css";
 
-class App extends Component {
-  render() {
+function App() {   
+
+    const [isAuthenticated, userHasAuthenticated] = useState(false);
+
     return (
-      <Router basename="/react-auth-ui/">
-        <Switch>
-        <div className="App">
-            <div className="appAside" />
-            <div className="appForm">
-                <div className="formTitle">               
-                    <Route path="/">
-                    <SignInForm />
-                    </Route>
+        <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated }}>
+            <Router basename="/react-auth-ui/">
+                <Switch>
+                <div className="App">
+                    <div className="appAside" />
+                    <div className="appForm">
+                        <div className="formTitle">               
+                            <Route path="/">
+                            <SignInForm />
+                            </Route>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-      </Switch>
-      </Router>
+                </Switch>
+            </Router>
+        </AppContext.Provider>
     );
-  }
 }
 
 export default App;
+
+// class App extends Component {
+    
+//     render() {
+//         return (
+//             <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated }}>
+//                 <Router basename="/react-auth-ui/">
+//                     <Switch>
+//                     <div className="App">
+//                         <div className="appAside" />
+//                         <div className="appForm">
+//                             <div className="formTitle">               
+//                                 <Route path="/">
+//                                 <SignInForm />
+//                                 </Route>
+//                             </div>
+//                         </div>
+//                     </div>
+//                     </Switch>
+//                 </Router>
+//             </AppContext.Provider>
+//         );
+//     }
+// }
+
+// export default App;
--- react-login-demo/src/pages/SignInForm.js
+++ react-login-demo/src/pages/SignInForm.js
@@ -1,85 +1,162 @@
 import React, { Component, useState } from "react";
 import { Auth } from "aws-amplify";
+import { useAppContext } from "../lib/contextLib";
 
-class SignInForm extends Component {
-  constructor() {
-    super();
 
-    this.state = {
-      email: "",
-      password: ""
+const SignInForm = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleChangeUsename = event => {
+        setEmail(event.target.value);  
+      };
+
+    const handleChangePassword = event => {
+        setPassword(event.target.value);  
     };
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+    const handleSubmit = () => {
+        (async () => {
+            console.log("The form was submitted with the following data:");
 
-  handleChange(event) {
-    let target = event.target;
-    let value = target.type === "checkbox" ? target.checked : target.value;
-    let name = target.name;
+            try {
+                await Auth.signIn(email, password);
+                alert("Logged in");
+            } catch (e) {
+                alert(e.message);
+            }
+        })();
+        console.log("bonjour");
+    };
 
-    this.setState({
-      [name]: value
-    });
-  }
-
-  async handleSubmit(event) {
-    event.preventDefault();
-
-    console.log("The form was submitted with the following data:");
-    console.log(this.state);
-
-    try {
-        await Auth.signIn(this.state.email, this.state.password);
-        alert("Logged in");
-      } catch (e) {
-        alert(e.message);
-      }
-  }
-
-  render() {
     return (
-      <div className="formCenter">
-        <form className="formFields" onSubmit={this.handleSubmit}>
-          <div className="formField">
+    <div className="formCenter">
+        <form className="formFields" onSubmit={handleSubmit}>
+        <div className="formField">
             <label className="formFieldLabel" htmlFor="email">
-              E-Mail Address
+            E-Mail Address
             </label>
             <input
-              type="email"
-              id="email"
-              className="formFieldInput"
-              placeholder="Enter your email"
-              name="email"
-              value={this.state.email}
-              onChange={this.handleChange}
+            type="email"
+            id="email"
+            className="formFieldInput"
+            placeholder="Enter your email"
+            name="email"
+            value={email}
+            onChange={handleChangeUsename}
             />
-          </div>
+        </div>
 
-          <div className="formField">
+        <div className="formField">
             <label className="formFieldLabel" htmlFor="password">
-              Password
+            Password
             </label>
             <input
-              type="password"
-              id="password"
-              className="formFieldInput"
-              placeholder="Enter your password"
-              name="password"
-              value={this.state.password}
-              onChange={this.handleChange}
+            type="password"
+            id="password"
+            className="formFieldInput"
+            placeholder="Enter your password"
+            name="password"
+            value={password}
+            onChange={handleChangePassword}
             />
-          </div>
+        </div>
 
-          <div className="formField">
-            <button onClick={this.testHook} className="formFieldButton">Sign In</button>{" "}
-          </div>
+        <div className="formField">
+            <button className="formFieldButton">Sign In</button>{" "}
+        </div>
 
         </form>
-      </div>
+    </div>
     );
-  }
 }
 
 export default SignInForm;
+
+// class SignInForm extends Component {
+//     constructor() {
+//       super();
+  
+//       this.state = {
+//         email: "",
+//         password: ""
+//       };
+  
+//       this.handleChange = this.handleChange.bind(this);
+//       this.handleSubmit = this.handleSubmit.bind(this);
+//     }
+  
+//     handleChange(event) {
+//       let target = event.target;
+//       let value = target.type === "checkbox" ? target.checked : target.value;
+//       let name = target.name;
+  
+//       this.setState({
+//         [name]: value
+//       });
+//     }
+  
+//     async handleSubmit(event) {
+//       event.preventDefault();
+  
+//       const { userHasAuthenticated } = useAppContext();
+  
+//       console.log("The form was submitted with the following data:");
+//       console.log(this.state);
+  
+//       try {
+//           await Auth.signIn(this.state.email, this.state.password);
+//           alert("Logged in");
+//           userHasAuthenticated(true);
+//         } catch (e) {
+//           alert(e.message);
+//         }
+//     }
+  
+//     render() {
+//       return (
+//         <div className="formCenter">
+//           <form className="formFields" onSubmit={this.handleSubmit}>
+//             <div className="formField">
+//               <label className="formFieldLabel" htmlFor="email">
+//                 E-Mail Address
+//               </label>
+//               <input
+//                 type="email"
+//                 id="email"
+//                 className="formFieldInput"
+//                 placeholder="Enter your email"
+//                 name="email"
+//                 value={this.state.email}
+//                 onChange={this.handleChange}
+//               />
+//             </div>
+  
+//             <div className="formField">
+//               <label className="formFieldLabel" htmlFor="password">
+//                 Password
+//               </label>
+//               <input
+//                 type="password"
+//                 id="password"
+//                 className="formFieldInput"
+//                 placeholder="Enter your password"
+//                 name="password"
+//                 value={this.state.password}
+//                 onChange={this.handleChange}
+//               />
+//             </div>
+  
+//             <div className="formField">
+//               <button onClick={this.testHook} className="formFieldButton">Sign In</button>{" "}
+//             </div>
+  
+//           </form>
+//         </div>
+//       );
+//     }
+//   }
+  
+//   export default SignInForm;
+  
