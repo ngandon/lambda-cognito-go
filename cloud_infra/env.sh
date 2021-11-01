335aa7493b09e319f526042d5e39dacaf97aef2a
ngandon <nicolas.gandon78@gmail.com>
2021-11-01 09:07:40 +0100

    Delete env.sh

--- cloud_infra/env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-export USER_EMAIL='user@minecraft-server.fr'
-export USER_PWD='Tiela38000_'
-export STACK_NAME='minecraft-server-cognito'
-export STACK_BUCKET='minecraft-server-cognito'
-export POOL_NAME="minecraftServerPool"
